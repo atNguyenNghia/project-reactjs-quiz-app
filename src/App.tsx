2c696c88ac1812c7efb90666830e633813b794d6
atNguyenNghia <atnguyennghia@gmail.com>
2022-03-11 20:55:53 -0800

    update app

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+// components
+import QuestionCard from './components/QuestionCard';
+
+
+const App = () => {
+
+
+  const startTrivia = async () => {
+
+  }
+  const checkAnswer = (e:React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+  const nextQuestion = () => {
+
+  }
+  return (
+    <div>
+      <h1>React Quiz</h1>
+      <button className='start' onClick={startTrivia}>
+        Start  
+      </button>
+      <p className='score'>Score: </p>
+      <p>Loading Questions ... </p>
+      <QuestionCard/>
+      <button className='next' onClick = {nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
+}
+
+export default App;
